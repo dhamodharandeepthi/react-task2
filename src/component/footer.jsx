e631a002bd603151985b7ae63438bf91c8a866ab
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2023-11-30 20:20:13 +0530

    sec commit

---
+++ src/component/footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <>
+            <div className="container-fluid bg-dark text-white ">
+                <div className="jumbotron p-5">
+                    <p>Copyright © Your Website 2023</p>
+                </div>
+
+            </div>
+
+        </>);
+}
+
+export default Footer;
